d3684a8d18262d7202a6117632e7d81a685f1a7f
geng-haoran <2000012975@stu.pku.edu.cn>
2024-07-17 07:15:21 +0800

    add stat better code

--- Method/utils/mesh.py
+++ Method/utils/mesh.py
@@ -1,11 +1,12 @@
 import trimesh
 
 # Load a mesh from OBJ file
-mesh = trimesh.load('path_to_mesh.obj')
+mesh = trimesh.load('/home/haoran/Projects/Rearrangement/Open6DOR/Method/assets/objaverse_final_norm/69511a7fad2f42ee8c4b0579bbc8fec6/material.obj')
 
 # Translate mesh to its centroid
 mesh.apply_translation(-mesh.centroid)
 
+import pdb; pdb.set_trace()
 # Scale the mesh (1 unit here)
 scale_factor = 1.0 / mesh.bounding_box.extents.max()
 mesh.apply_scale(scale_factor)
